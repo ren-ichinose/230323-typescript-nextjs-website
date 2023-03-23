e93fba216bb40c3a25071eed530ff8b783d25c84
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-23 13:25:02 +0900

    Pagesの準備 #1

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <h1>hello world</h1>;
+  return (
+    <>
+      <h1>hello world</h1>
+      <Link href={'/contact'}>Contact</Link>
+    </>
+  );
 }

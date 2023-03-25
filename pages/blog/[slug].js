acf041cbce54cd66b6a7fd0a5b696a54db45a84a
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-25 22:13:12 +0900

    getStaticPropsの実装 #3

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,4 +1,6 @@
-const ShingleBlog = () => {
+import matter from 'gray-matter';
+
+const ShingleBlog = ({ frontmatter, markdownBody }) => {
   return <h1>記事ページ</h1>;
 };
 
@@ -20,5 +22,17 @@ export async function getStaticPaths() {
   return {
     paths,
     fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { slug } = context.params;
+  const blogData = await import(`../../data/${slug}.md`);
+  const { data, content } = matter(blogData.default);
+  return {
+    props: {
+      frontmatter: data,
+      markdownBody: content,
+    },
   };
 }

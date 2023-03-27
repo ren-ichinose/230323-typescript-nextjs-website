e64e8e635d8714af698aa6dfe7ba691e851daf2d
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-27 14:00:42 +0900

    スタイリングの仮調整

--- components/footer.js
+++ components/footer.js
@@ -1,20 +1,28 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <footer>
       <div>
         <a href="https://www.google.com/">
-          <img src="/images/github.svg" alt="logo" />
+          <img src="/images/github.svg" alt="logo" style={{ width: '50px' }} />
         </a>
         <a href="https://www.google.com/">
-          <img src="/images/linkedin.svg" alt="logo" />
+          <img
+            src="/images/linkedin.svg"
+            alt="logo"
+            style={{ width: '50px' }}
+          />
         </a>
         <a href="https://www.google.com/">
-          <img src="/images/twitter.svg" alt="logo" />
+          <img src="/images/twitter.svg" alt="logo" style={{ width: '50px' }} />
         </a>
         <a href="https://www.google.com/">
-          <img src="/images/facebook.svg" alt="logo" />
+          <img
+            src="/images/facebook.svg"
+            alt="logo"
+            style={{ width: '50px' }}
+          />
         </a>
         <hr />
         <Link href="/blog">Blog</Link>
--- pages/blog.js
+++ pages/blog.js
@@ -12,22 +12,22 @@ const Blog = ({ Blogs }) => {
           <p>エンジニアの日常生活をお届けします</p>
           {Blogs.map(({ frontmatter, slug }) => {
             return (
-              <div key={slug}>
+              <div key={slug} style={{ display: 'flex' }}>
+                <div>
+                  <Image
+                    src={frontmatter.image}
+                    alt="card-image"
+                    height={250}
+                    width={420}
+                    quality={90}
+                    priority
+                  />
+                </div>
                 <div>
                   <h3>{frontmatter.title}</h3>
                   <p>{frontmatter.excerpt}</p>
                   <p>{frontmatter.date}</p>
                   <Link href={`/blog/${slug}`}>Read More</Link>
-                </div>
-                <div>
-                  <Image
-                    src={frontmatter.image}
-                    alt="card-image"
-                    height={300}
-                    width={520}
-                    quality={90}
-                    priority
-                  />
                 </div>
               </div>
             );

d38364a1b4b60bdc92ec09854d714993e6272fc7
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-27 12:50:47 +0900

    404ページの実装

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <div style={{ textAlign: 'center', height: '70vh' }}>
+      <h1>401:NotFoundPage</h1>
+      <p>ページが見つかりません</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;

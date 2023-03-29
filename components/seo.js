96c0922f38bda15d572ad8c03b8cbb9352c533d7
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-29 11:46:14 +0900

    seoコンポーネントの実装 #8

---
+++ components/seo.js
@@ -0,0 +1,33 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+const Seo = ({ title, description }) => {
+  const router = useRouter();
+  const baseUrl = 'http://localhost:3000';
+  const currentUrl = baseUrl + router.pathname;
+  const defaultImage = `${baseUrl}/images/social-card.png`;
+  return (
+    <Head>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta charSet="utf-8" />
+      
+      <title>{title}</title>
+      <meta name="description" content={description} key="description" />
+      <meta name="image" content={defaultImage} key="image" />
+      <link rel="canonical" href={currentUrl} key="canonical" />
+
+      <meta property="og:title" content={title} key="ogtitle" />
+      <meta
+        property="og:description"
+        content={description}
+        key="ogdescription"
+      />
+      <meta property="og:image" content={defaultImage} key="ogimage" />
+      <meta property="og:url" content={currentUrl} key="ogurl" />
+
+      <link rel="shortcut icon" href="/images/favicon.ico" />
+    </Head>
+  );
+};
+
+export default Seo;
